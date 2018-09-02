137cd78ea8319cdd82485979bb9eee84bb2d0aa2
winton <mail@wintoni.us>
2018-09-02 04:56:04 -0400

    Parse args test

---
+++ test/parseArgsTest.js
@@ -0,0 +1,93 @@
+import DotEvent from "../dist/core"
+import { parseArgs } from "../dist/args"
+
+describe("parse args", () => {
+  test("no args", () => {
+    const emitter = new DotEvent()
+    const options = parseArgs({ args: [], emitter })
+    expect(options).toEqual({
+      emitter: expect.any(DotEvent),
+      key: "::",
+    })
+  })
+
+  test("function", () => {
+    const emitter = new DotEvent()
+    const options = parseArgs({ args: [() => {}], emitter })
+    expect(options).toEqual({
+      emitter: expect.any(DotEvent),
+      fn: expect.any(Function),
+      key: "::",
+    })
+  })
+
+  test("options", () => {
+    const emitter = new DotEvent()
+    const options = parseArgs({
+      args: [{ hello: "world" }],
+      emitter,
+    })
+    expect(options).toEqual({
+      emitter: expect.any(DotEvent),
+      key: "::",
+      options: { hello: "world" },
+    })
+  })
+
+  test("before/after", () => {
+    const emitter = new DotEvent()
+    expect(
+      parseArgs({
+        args: ["before"],
+        emitter,
+      })
+    ).toEqual({
+      emitter: expect.any(DotEvent),
+      key: "before::",
+      prep: "before",
+    })
+  })
+
+  test("op", () => {
+    const emitter = new DotEvent()
+    emitter.op("create")
+    expect(
+      parseArgs({
+        args: ["create"],
+        emitter,
+      })
+    ).toEqual({
+      emitter: expect.any(DotEvent),
+      key: ":create:",
+      op: "create",
+    })
+  })
+
+  test("props", () => {
+    const emitter = new DotEvent()
+    expect(
+      parseArgs({
+        args: ["hello"],
+        emitter,
+      })
+    ).toEqual({
+      emitter: expect.any(DotEvent),
+      key: "::hello",
+      props: "hello",
+    })
+  })
+
+  test("extras", () => {
+    const emitter = new DotEvent()
+    expect(
+      parseArgs({
+        args: [true, false, 123, null, []],
+        emitter,
+      })
+    ).toEqual({
+      emitter: expect.any(DotEvent),
+      extras: [true, false, 123, null, []],
+      key: "::",
+    })
+  })
+})

b5ef8aae3d87ae0c3b3f300ba3854283fc8d44b6
winton <mail@wintoni.us>
2018-09-06 00:45:21 -0400

    More wildcard tests

--- test/propsTest.js
+++ test/propsTest.js
@@ -21,6 +21,25 @@ describe("props", () => {
       expect(fn.mock.calls).toEqual([[payload]])
     })
 
+    test("one wildcard emit", async () => {
+      const event = new DotEvent()
+      const fn = jest.fn()
+
+      event.on("hello.*", fn)
+
+      await event.emit("hello.world").catch(console.error)
+
+      const payload = {
+        event: {
+          emitter: expect.any(DotEvent),
+          props: "hello.world",
+          propsArray: ["hello", "world"],
+        },
+      }
+
+      expect(fn.mock.calls).toEqual([[payload]])
+    })
+
     test("two emits", async () => {
       const event = new DotEvent()
       const fn = jest.fn()
@@ -79,6 +98,35 @@ describe("props", () => {
           emitter: expect.any(DotEvent),
           props: "hello.world.again",
           propsArray: ["hello", "world", "again"],
+        },
+      }
+
+      expect(fn.mock.calls).toEqual([[payload], [payload2]])
+    })
+
+    test("two wildcard emits", async () => {
+      const event = new DotEvent()
+      const fn = jest.fn()
+
+      event.onAny("*", fn)
+
+      await event.emit().catch(console.error)
+      await event.emit("hello").catch(console.error)
+      await event.emit("hello.world").catch(console.error)
+
+      const payload = {
+        event: {
+          emitter: expect.any(DotEvent),
+          props: "hello",
+          propsArray: ["hello"],
+        },
+      }
+
+      const payload2 = {
+        event: {
+          emitter: expect.any(DotEvent),
+          props: "hello.world",
+          propsArray: ["hello", "world"],
         },
       }
 

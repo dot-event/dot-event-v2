9c9447282fd61153696d0cabc217ac607cec4de3
winton <mail@wintoni.us>
2018-09-20 13:47:23 -0400

    Adding test to see what sync errors do

--- test/syncTest.js
+++ test/syncTest.js
@@ -22,5 +22,26 @@ describe("sync", () => {
 
       expect(order).toEqual([1, 2])
     })
+
+    test("one emit with error", async () => {
+      const events = new Events()
+
+      const fn = async () => {
+        throw new Error("error")
+      }
+
+      const fn2 = jest.fn()
+
+      events.sync().on(fn)
+      events.sync().on(fn2)
+
+      try {
+        await events.emit()
+      } catch (e) {
+        null
+      }
+
+      expect(fn2.mock.calls.length).toBe(0)
+    })
   })
 })
